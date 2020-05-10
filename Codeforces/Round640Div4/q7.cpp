a17b170a4801604870b5b53aabb3c13eb4dd0edf
Rajbir Singh <singh2013rajbir@gmail.com>
2020-05-10 18:17:10 -0400

    Round640Div4

--- Codeforces/Round640Div4/q7.cpp
+++ Codeforces/Round640Div4/q7.cpp
@@ -12,24 +12,53 @@ int main()
   freopen("input.txt","r",stdin);
   vector<int> a;
   cin>>t;
-  for(i=0; i<t; i++){
+  int b[5] = {3,1,4,2,5};
+  for(k=0; k<t; k++){
     cin>>n;
+    int a[n];
     if(n<=3){
-      cout<<"-1"<<endl;
+      cout<<"-1";
     }
     else if(n==4){
-      cout<<"3 1 4 2"<<endl;
+      cout<<"3 1 4 2";
     }
     else{
-      for(j=1;j<=n;){
-        cout<<j<<" ";
-        j = j +2;
+      int q = n/5;
+      for(i=0;i<q;i++){
+        for(j=0;j<5;j++){
+          a[5*i+j] = (5*i + b[j]);
+        }
       }
-      for(j=2;j<=n;){
-        cout<<j<<" ";
-        j = j +2;
+      if(n%5==1){
+        a[n-1] = n-1;
+        a[n-2] = n-4;
+        a[n-3] = n;
       }
-      cout<<endl;
+      else if(n%5==2){
+        a[n-1] = n;
+        a[n-2] = n-2;
+        a[n-3] = n-5;
+        a[n-4] = n-1;
+      }
+      else if(n%5==3){
+        a[n-1] = n-1;
+        a[n-2] = n-3;
+        a[n-3] = n-6;
+        a[n-4] = n-2;
+        a[n-5] = n;
+      }
+      else if(n%5==4){
+        a[n-1] = n;
+        a[n-2] = n-2;
+        a[n-3] = n-4;
+        a[n-4] = n-7;
+        a[n-5] = n-3;
+        a[n-6] = n-1;
+      }
+      for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+      }
     }
+      cout<<endl;
   }
 }
